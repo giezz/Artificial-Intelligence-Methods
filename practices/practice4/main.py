37039f552d6df3cd6cfd8947208a545109b32e23
giezz <giezz@vk.com>
2022-11-21 16:50:36 +0300

    practice 4 fix data points selection

--- practices/practice4/main.py
+++ practices/practice4/main.py
@@ -6,16 +6,14 @@ def print_plots(film1, film2):
     x = []
     y = []
     for key, value in critics.items():
-        for key1, value1 in value.items():
-            if key1 in film1:
-                x.append(value1)
-            elif key1 in film2:
-                y.append(value1)
+        if film1 in value and film2 in value:
+            x.append(value[film1])
+            y.append(value[film2])
+        else:
+            continue
 
-    if len(x) > len(y):
-        x = x[:len(y)]
-    else:
-        y = y[:len(x)]
+    print(x)
+    print(y)
 
     fig, ax = plt.subplots()
     ax.scatter(x, y)
@@ -25,7 +23,7 @@ def print_plots(film1, film2):
 
 
 if __name__ == '__main__':
-    print_plots('Каникулы в Простоквашино', 'Ёжик в тумане')
+    print_plots('Зима в Простоквашино', 'Ёжик в тумане')
     print(sim_distance(critics, 'Кот Матроскин', 'Пёс Шарик'))
     print(sim_pearson(critics, 'Кот Матроскин', 'Пёс Шарик'))
     top_matches(critics, 'Кот Матроскин')
